c7bf0985147ea9ef909f861b187d04b29f8905d5
Rishik <rishikgoyal00@gmail.com>
2021-04-24 02:19:08 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+  
+var app = require('express')();
+var http = require('http').createServer(app);
+// server-side
+const io = require("socket.io")(http, {
+  cors: {
+    origin: '*',
+    methods: ["GET", "POST"],
+    credentials: true
+  }
+});
+
+io.on('connection', (socket)=> {
+      console.log('User Online');
+
+      socket.on('canvas-data', (data)=> {
+            socket.broadcast.emit('canvas-data', data);
+            
+      })
+})
+
+var server_port = process.env.YOUR_PORT || process.env.PORT || 5000;
+http.listen(server_port, () => {
+    console.log("Started on : "+ server_port);
+})
